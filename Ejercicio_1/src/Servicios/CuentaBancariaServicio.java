d43845d381286c5e5364036b8932165a306d7924
sebacozzi <127224339+sebacozzi@users.noreply.github.com>
2023-04-03 21:47:25 -0300

    Ejercicio 1 - Cargado con menu de opciones y clases probadas

--- Ejercicio_1/src/Servicios/CuentaBancariaServicio.java
+++ Ejercicio_1/src/Servicios/CuentaBancariaServicio.java
@@ -1,9 +1,9 @@
-/**Realizar una clase llamada CuentaBancaria en el paquete Entidades con los 
- * siguientes atributos: numeroCuenta(entero), dniCliente(entero largo), 
+/** Realizar una clase llamada CuentaBancaria en el paquete Entidades con los
+ * siguientes atributos: numeroCuenta(entero), dniCliente(entero largo),
  * saldoActual. Agregar constructor vacío, con parámetros, getters y setters.
-    Agregar la clase CuentaBancariaServicio en el paquete Servicios que contenga:
+ * Agregar la clase CuentaBancariaServicio en el paquete Servicios que contenga:
  *  a) Método para crear cuenta pidiéndole los datos al usuario.
- *  b) Método ingresar(double): recibe una cantidad de dinero a ingresar y se 
+ *  b) Método ingresar(double): recibe una cantidad de dinero a ingresar y se
  *      le sumará al saldo actual.
  *  C) Método retirar(double): recibe una cantidad de dinero a retirar y se le
  *      restara al saldo actual. Si la cuenta no tiene la cantidad de dinero
@@ -23,22 +23,112 @@ import java.util.Scanner;
  * @author Sebastián Cozzi
  */
 public class CuentaBancariaServicio {
-    
+
     // inicialización de Scanner
     private final Scanner leer = new Scanner(System.in).useDelimiter("\n");
-    
-    
-    public CuentaBancaria crearCuenta(){
+
+    /**
+     * a) Método para crear cuenta pidiéndole los datos al usuario.
+     *
+     * @return
+     */
+    public CuentaBancaria crearCuenta() {
         System.out.print("Ingrese el número de cuenta del cliente: ");
-        int numeroCuenta=leer.nextInt();
-        
+        int numeroCuenta = leer.nextInt();
+
         System.out.print("Ingrese el número de DNI del cliente: ");
-        long numeroDNI=leer.nextLong();
-        
+        long numeroDNI = leer.nextLong();
+
         System.out.print("Ingrese el saldo inicial de la cuenta: ");
-        double saldoActual=leer.nextDouble();
-        
-        return new CuentaBancaria(numeroCuenta,numeroDNI,saldoActual);
+        double saldoActual = leer.nextDouble();
+
+        return new CuentaBancaria(numeroCuenta, numeroDNI, saldoActual);
+    }
+
+    /**
+     * b) Método ingresar(double): recibe una cantidad de dinero a ingresar y se
+     * le sumará al saldo actual.
+     *
+     * @param cuenta
+     *          Variable del tipo CuentaBancaria
+     *              con los datos del cliente 
+     * @param monto
+     *          Variable del tipo double
+     *              con el monto que va a depositar en la cuenta
+     */
+    public void ingresar(CuentaBancaria cuenta, double monto) {
+        cuenta.setSaldoActual(cuenta.getSaldoActual() + monto);
+    }
+
+    /**
+     * C) Método retirar(double): recibe una cantidad de dinero a retirar y se
+     * le restara al saldo actual.Si la cuenta no tiene la cantidad de dinero a
+ retirar se retirará el máximo posible hasta dejar la cuenta en 0.
+     *
+     * @param cuenta 
+     *          Variable del tipo CuentaBancaria
+     *              con los datos del cliente
+     * @param monto 
+     *          Variable del tipo double
+     *              con el monto a retirar de la cuenta
+     */
+    public void retirar(CuentaBancaria cuenta, double monto){
+        if (cuenta.getSaldoActual()<monto) {
+            System.out.println("El cliente solo puede retirar $ "+ cuenta.getSaldoActual()+".");
+            cuenta.setSaldoActual(cuenta.getSaldoActual());
+        }else {
+            cuenta.setSaldoActual(cuenta.getSaldoActual()-monto);
+            System.out.println("El cliente retira $ "+ monto+".");
+            System.out.println("El saldo en cuents es de $ "+cuenta.getSaldoActual()+".");
+        }        
     }
     
+    /**
+     * d) Método extraccionRapida: le permitirá sacar solo un 20% de su saldo.
+     *         Validar que el usuario no saque más del 20%.
+     * 
+     * @param cuenta
+     *          Variable del tipo CuentaBancaria
+     *              con los datos del cliente
+     * @param monto
+     *          Variable del tipo double
+     *              con el monto a retirar de la cuenta con el limite
+     *              maximo del 20%
+     */
+    
+    public void extraccionRapida(CuentaBancaria cuenta, double monto){
+        double maximo=cuenta.getSaldoActual()*0.2;
+        if (maximo>=monto) {
+            System.out.println("El cliente retira $"+ monto+".");
+            cuenta.setSaldoActual(cuenta.getSaldoActual()- monto);
+        }else{
+            System.out.println("El cliente solo puede retirar $"+ maximo+", correspondiente al 20% del saldo.");
+            cuenta.setSaldoActual(cuenta.getSaldoActual()-maximo);
+        }
+    }
+    
+    /**
+     * e) Método consultarSaldo: permitirá consultar el saldo disponible en la cuenta.
+     * 
+     * @param cuenta
+     *          Variable del tipo CuentaBancaria
+     *              con los datos del cliente
+     */
+    public void consultarSaldo(CuentaBancaria cuenta){
+        System.out.println("El saldo del cliente es de $ "+cuenta.getSaldoActual()+".");
+    }
+    
+    /**
+     *   f) Método consultarDatos: permitirá mostrar todos los datos de la cuenta.
+     * 
+     * @param cuenta
+     *          Variable del tipo CuentaBancaria
+     *              con los datos del cliente 
+     */
+    public void consultarDatos(CuentaBancaria cuenta){
+        System.out.println("Datos del cliente:\n"+
+                           "DNI del Cliente: " + cuenta.getDniCliente()+"\n"+
+                           "Número de cuenta: "+ cuenta.getNumeroCuenta()+"\n"+
+                           "Saldo en cuenta: "+cuenta.getSaldoActual());
+    }
 }
--- Ejercicio_1/src/ejercicio_1/Ejercicio_1.java
+++ Ejercicio_1/src/ejercicio_1/Ejercicio_1.java
@@ -15,6 +15,11 @@
  */
 package ejercicio_1;
 
+import Entidad.CuentaBancaria;
+import Servicios.CuentaBancariaServicio;
+import java.util.Scanner;
+import menu.Menu;
+
 /**
  *
  * @author Sebastián Cozzi
@@ -25,7 +30,57 @@ public class Ejercicio_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // inicialización de Scanner
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        CuentaBancariaServicio scb=new CuentaBancariaServicio();
+        CuentaBancaria cuenta = null;
+        
+        String op1[]={"Cargar Cuenta(a)","Ingresar dinero(b)","Extracción(c)", "Extracción Rapida(d)", "Consultar Saldo(e)","Datos del cliente(f)"};
+        boolean salida =true;
+        
+        Menu menu =new Menu("Menu Principal:",op1, "Salir");
+        
+        do {
+            System.out.println( menu.toString());
+            if (menu.pregunta()!=1 && cuenta == null) {
+                System.out.println("Primero debe crear la cuenta.");
+            } else {
+            switch (menu.getResultado()) {
+                case 1:// Cargar Cuenta
+                    cuenta= scb.crearCuenta();
+                    menu.esperaTecla();
+                    break;
+                case 2:// Ingresar Dinero
+                    System.out.print("Ingrese el monto a depositar: ");
+                    scb.ingresar(cuenta, leer.nextDouble());
+                    break;
+                case 3: // Extracción
+                    System.out.print("Ingrese el monto a retirar: ");
+                    scb.retirar(cuenta, leer.nextDouble());
+                    break;
+                case 4:// Extracción Rapida
+                    System.out.print("Ingrese el monto a depositar: ");
+                    scb.extraccionRapida(cuenta, leer.nextDouble());
+                    break;
+                case 5: // Consultar Saldo
+                    scb.consultarSaldo(cuenta);
+                    break;
+                case 6:
+                    scb.consultarDatos(cuenta);
+                    break;
+                case 7:
+                    salida= false;
+                    System.out.println("Gracias por usar nuestros servicios!!");
+                    break;
+                default:
+                    System.out.println("opcion invalida - es de 1 a 6.");
+            }
+                if (menu.getResultado()>0 && menu.getResultado()<=6) {
+                    menu.esperaTecla();
+                }
+            }
+        } while (salida);
+        
     }
     
 }

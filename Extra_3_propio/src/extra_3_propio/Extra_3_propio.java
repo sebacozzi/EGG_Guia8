eef702ae266fc23e06dd193eb3923c24fdadb2f9
sebacozzi <127224339+sebacozzi@users.noreply.github.com>
2023-04-12 20:10:44 -0300

    actualizaciones

--- Extra_3_propio/src/extra_3_propio/Extra_3_propio.java
+++ Extra_3_propio/src/extra_3_propio/Extra_3_propio.java
@@ -1,47 +1,46 @@
-/**ejercicio 2, se te pide que crees una clase de entidad para representar una 
+/** ejercicio 2, se te pide que crees una clase de entidad para representar una
  * cuenta bancaria y una clase de servicio para manejar la lógica empresarial
  * de la gestión de cuentas bancarias. Aquí hay una explicación general de cómo
  * podrías hacerlo:
-
-    Clase de entidad:
-        La clase de entidad "CuentaBancaria" tendría las siguientes propiedades:
-    Número de cuenta: un número único que identifica la cuenta bancaria.
-    Nombre del titular de la cuenta: el nombre completo de la persona que posee la cuenta.
-    Saldo actual: el saldo disponible en la cuenta bancaria.
-
-*   Clase de servicio:
-    La clase de servicio "GestorCuentasBancarias" tendría métodos para crear una
-    * nueva cuenta bancaria, realizar una transferencia de fondos entre dos 
-    * cuentas y obtener el saldo actual de una cuenta bancaria. 
-    * Aquí hay un ejemplo básico:
-        Crear cuenta bancaria: Este método recibiría el nombre del titular de 
-        * la cuenta y un saldo inicial opcional. La implementación de este 
-        * método crearía una nueva cuenta bancaria con un número de cuenta único
-        * y agregaría la cuenta a una lista de cuentas bancarias mantenida por
-        * la clase "GestorCuentasBancarias".
-        Transferir fondos: Este método recibiría el número de cuenta de la 
-        * cuenta de origen, el número de cuenta de la cuenta de destino y el 
-        * monto de la transferencia. La implementación de este método buscaría
-        * ambas cuentas en la lista de cuentas bancarias y transferiría el 
-        * monto especificado de la cuenta de origen a la cuenta de destino,
-        * actualizando los saldos de ambas cuentas.
-        Obtener saldo: Este método recibiría el número de cuenta de la cuenta
-        * bancaria y devolvería el saldo actual de la cuenta.
-    Implementación en el método principal:
-        En el método principal, podrías crear una instancia de la clase 
-        * "GestorCuentasBancarias" y luego llamar a los métodos de la clase 
-        * para crear cuentas bancarias, transferir fondos y obtener saldos de
-        * cuenta. Por ejemplo, podrías crear dos cuentas bancarias y transferir
-        * fondos entre ellas:
-    Crear una cuenta bancaria para "Juan Pérez" con un saldo inicial de $1000.
-    Crear una cuenta bancaria para "María Gómez" con un saldo inicial de $500.
-    Transferir $500 de la cuenta bancaria de "Juan Pérez" a la cuenta bancaria de "María Gómez".
-    Obtener el saldo actual de la cuenta bancaria de "Juan Pérez" y mostrarlo en la pantalla.
+ *
+ * Clase de entidad:
+ * La clase de entidad "CuentaBancaria" tendría las siguientes propiedades:
+ * Número de cuenta: un número único que identifica la cuenta bancaria.
+ * Nombre del titular de la cuenta: el nombre completo de la persona que posee la cuenta.
+ * Saldo actual: el saldo disponible en la cuenta bancaria.
+ *
+ *   Clase de servicio:
+ * La clase de servicio "GestorCuentasBancarias" tendría métodos para crear una
+ * nueva cuenta bancaria, realizar una transferencia de fondos entre dos
+ * cuentas y obtener el saldo actual de una cuenta bancaria.
+ * Aquí hay un ejemplo básico:
+ * Crear cuenta bancaria: Este método recibiría el nombre del titular de
+ * la cuenta y un saldo inicial opcional. La implementación de este
+ * método crearía una nueva cuenta bancaria con un número de cuenta único
+ * y agregaría la cuenta a una lista de cuentas bancarias mantenida por
+ * la clase "GestorCuentasBancarias".
+ * Transferir fondos: Este método recibiría el número de cuenta de la
+ * cuenta de origen, el número de cuenta de la cuenta de destino y el
+ * monto de la transferencia. La implementación de este método buscaría
+ * ambas cuentas en la lista de cuentas bancarias y transferiría el
+ * monto especificado de la cuenta de origen a la cuenta de destino,
+ * actualizando los saldos de ambas cuentas.
+ * Obtener saldo: Este método recibiría el número de cuenta de la cuenta
+ * bancaria y devolvería el saldo actual de la cuenta.
+ * Implementación en el método principal:
+ * En el método principal, podrías crear una instancia de la clase
+ * "GestorCuentasBancarias" y luego llamar a los métodos de la clase
+ * para crear cuentas bancarias, transferir fondos y obtener saldos de
+ * cuenta. Por ejemplo, podrías crear dos cuentas bancarias y transferir
+ * fondos entre ellas:
+ * Crear una cuenta bancaria para "Juan Pérez" con un saldo inicial de $1000.
+ * Crear una cuenta bancaria para "María Gómez" con un saldo inicial de $500.
+ * Transferir $500 de la cuenta bancaria de "Juan Pérez" a la cuenta bancaria de "María Gómez".
+ * Obtener el saldo actual de la cuenta bancaria de "Juan Pérez" y mostrarlo en la pantalla.
  */
 package extra_3_propio;
 
-import Servicios.ServiciosMenu;
-import Utilidades.Menu;
+import menuDeOpciones;
 
 /**
  *
@@ -54,12 +53,53 @@ public class Extra_3_propio {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String[] ops={"Opcion 1", "Opcion 2", "Opcion 3", "Opcion 4", "Opcion 5", "Opcion 6"};
-        String[] op={"Opcion 1", "Opcion 2", "Opcion 3"};
-        Menu menu = new Menu(ops, "Menu de opciones");
-        ServiciosMenu sm=new ServiciosMenu(); 
-        sm.show(new Menu(op,"menu 2"));
-               
+        String[] op1 = {"Opcion 1", "Opcion 2", "Opcion 3", "Opcion 4", "Opcion 5", "Volver"};
+        String[] op0 = {"Opcion 1", "Opcion 2", "Salir"};
+        String[] op2 = {"Opcion 4", "Opcion 5", "Volver"};
+        ServiciosMenu sm = new ServiciosMenu();
+        boolean salida;
+        do {
+            salida = true;
+            sm.show(new Menu(op0, "menu Principal"));
+            switch (sm.getResultado()) {
+                case 1:
+                    do {
+                        sm.show(new Menu(op1, "Menu Opción 1"));
+                        switch (sm.getResultado()) {
+                            case 1:
+                                System.out.println("Opción 1 elejida.");
+                                break;
+                            case 2:
+                                System.out.println("Opción 2 elejida.");
+                                break;
+                            case 3:
+                                System.out.println("Opción 3 elejida.");
+                                break;
+                            case 4:
+                                System.out.println("Opción 4 elejida.");
+                                break;
+                            case 5:
+                                System.out.println("Opción 5 elejida.");
+                                break;
+                            case 6:
+                                System.out.println("Volver al menu principal");
+                        }
+                        sm.esperaTecla();
+                        if (sm.getResultado() == 6) {
+                            break;
+                        }
+                    } while (true);
+                    break;
+                case 2:
+                    sm.show(new Menu(op2, "Menu Opción 2"));
+                    System.out.println("Los resultados de las opciones no estan controladas... vuelve al menu principal.");
+                    sm.esperaTecla();
+                    break;
+                case 3:
+                    salida = false;
+            }
+        } while (salida);
+
     }
-    
+
 }
